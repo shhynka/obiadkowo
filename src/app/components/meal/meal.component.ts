add51b63d876de9526c89a882ed4a123e7180c51
Shhynka <iga.popielarz@gmail.com>
2021-06-04 12:31:00 +0200

    moved delete button from meal view to meal card in list, changed its style

--- src/app/components/meal/meal.component.ts
+++ src/app/components/meal/meal.component.ts
@@ -1,5 +1,11 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ActivatedRoute } from '@angular/router';
+import { EMPTY } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 import { Meal } from 'src/app/models/meal.model';
+import { MealService } from 'src/app/services/meal.service';
+import { DeleteMealDialogComponent } from '../delete-meal-dialog/delete-meal-dialog.component';
 
 @Component({
   selector: 'app-meal',
@@ -10,9 +16,21 @@ export class MealComponent implements OnInit {
 
   @Input() layoutType: 'list' | 'gallery';
   @Input() meal: Meal;
-  constructor() { }
+
+  constructor(private matDialog: MatDialog, private mealService: MealService) { }
 
   ngOnInit(): void {
+  }
+
+  openDeleteDialog() {
+    let dialogRef = this.matDialog.open(DeleteMealDialogComponent);
+
+    dialogRef.afterClosed().pipe(switchMap(result => {
+      if (result) {
+        return this.mealService.deleteMeal(this.meal.id);
+      }
+      return EMPTY;
+    })).subscribe();
   }
 
 }
